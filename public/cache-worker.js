a8ae1748947c34e1b1acfd4fbb5181c85478cbd3
RickyCohen <rickycohen88@gmail.com>
2021-02-18 10:50:25 -0600

    adding code to data worker

--- public/cache-worker.js
+++ public/cache-worker.js
@@ -12,7 +12,7 @@ const FILES_TO_CACHE = [
 ];
 
 const STATIC_CACHE = "static-cache-v1";
-const DATA_CACHE_NAME = "data-cache-v1";
+
 //install
 self.addEventListener("install", event => {
     event.waitUntil(
---
+++ public/data-worker.js
@@ -0,0 +1,44 @@
+const DATA_CACHE_NAME = "data-cache-v1";
+
+//install
+self.addEventListener("install", event => {
+    event.waitUntil(
+      caches
+        .open(STATIC_CACHE)
+        .then(cache => cache.addAll(FILES_TO_CACHE))
+        .then(() => self.skipWaiting())
+    );
+  });
+  
+
+  // activate
+self.addEventListener("activate", function(evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== STATIC_CACHE && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+
+// fetch
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request);
+      })
+    );
+  }
+});
