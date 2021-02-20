0759b6bfbaa8b0b2813661f04106d2f8aa92be39
RickyCohen <rickycohen88@gmail.com>
2021-02-19 21:07:05 -0600

    adding error logging to mongoose connection

--- server.js
+++ server.js
@@ -19,7 +19,11 @@ mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost:27017/budget', {
   useNewUrlParser: true,
   useFindAndModify: false
 });
-
+const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function () {
+        console.log("connected to DB");
+    });
 // routes
 app.use(require("./routes/api.js"));
 
